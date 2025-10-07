2d06dca95a7cafc80e84a0d576f39575254532ca
Minyguy <benjaminleikvold@gmail.com>
2025-10-07 10:59:57 +0200

    platformIO

--- main.cpp
+++
@@ -1,8 +0,0 @@
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
-
--- workspace/main.cpp
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-        2
-    }
-}
-
