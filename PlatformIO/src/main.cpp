213a44c64e36f0804e351120e3110fa2b66a175e
Fred <100497165+SilverBulett@users.noreply.github.com>
2025-10-14 12:46:23 +0200

    initial flash build

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -1,16 +1,22 @@
-// put function declarations here:
-int myFunction(int, int);
+#include <avr/io.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int main(){
+
+// Set PC5 (pin 28 on PDIP) as output
+    DDRC |= (1 << DDC5);
+
+    while (1) {
+        // Turn LED ON (PC5 HIGH)
+        PORTC |= (1 << PC5);
+        _delay_ms(50);
+
+        // Turn LED OFF (PC5 LOW)
+        PORTC &= ~(1 << PC5);
+        _delay_ms(50);
+    }
+  return 0;
 }
